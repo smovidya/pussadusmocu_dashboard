8de04319d593a12a93d7c7fb81a9dd0749992893
banyaphon <92526947+bypkt-bk@users.noreply.github.com>
2024-10-11 02:00:09 +0700

    ci and nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,37 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow01: "#F7CF47",
+        yellow02: "#DAB021",
+        yellow03: "#C39800",
+        white01: "#FFFFFF",
+        green01: "#219847",
+        red01: "#C30016",
+        grey01: "#6B6B6B",
+        black01: "#000000",
+        grey02: "#F2F2F2",
+      },
+      fontFamily: {
+        "noto-sans": ["var(--font-sans)"],
+      },
+      fontSize: {
+        H1: "64px",
+        H2: "58px",
+        H3: "24px",
+        subHead1: "32px",
+        subHead2: "28px",
+        body1: "18px",
+        body2: "16px",
+        body3: "12px",
+        body4: "10px",
+        subHeadEng1: "20px",
+        bodyEngBold1: "16px",
+        bodyEng2: "16px",
+        bodyEng3: "12px",
+      },
+    },
   },
   plugins: [],
 }
